d2ec8253a780c00a8060e3fb3e03a0787b948d63
Manish <manishrajak497229@gmail.com>
2025-05-03 08:58:41 +0530

    render_API_base_URL implemented

--- frontend/src/pages/home/Banner.jsx
+++ frontend/src/pages/home/Banner.jsx
@@ -11,7 +11,7 @@ const Banner = () => {
         
         <div className='md:w-1/2 w-full'>
             <h1 className='md:text-5xl text-2xl font-medium mb-7'>Books are everything</h1>
-            <p className='mb-10'>ilent companions that enlighten, entertain, and educate. They preserve knowledge, culture, and human experience across time. Whether fiction or fact, books spark imagination, shape perspectives, and nurture curiosity. They offer escape from reality and also help us face it with wisdom. In every chapter, they build empathy, inspire thought, and guide us toward personal and collective growth.for everyone
+            <p className='mb-10'>Silent companions that enlighten, entertain, and educate. They preserve knowledge, culture, and human experience across time. Whether fiction or fact, books spark imagination, shape perspectives, and nurture curiosity. They offer escape from reality and also help us face it with wisdom. In every chapter, they build empathy, inspire thought, and guide us toward personal and collective growth.for everyone
 
             </p>
 <p className='mb-10'>For new updates 👇</p>
