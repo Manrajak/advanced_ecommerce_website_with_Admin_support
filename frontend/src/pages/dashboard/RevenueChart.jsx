9205d3c7960bba25e18f0450a3b6d46cb34b5202
Manish <manishrajak497229@gmail.com>
2025-06-21 21:58:26 +0530

    sub_final commit

--- frontend/src/pages/dashboard/RevenueChart.jsx
+++ frontend/src/pages/dashboard/RevenueChart.jsx
@@ -1,15 +1,11 @@
-// src/components/RevenueChart.jsx
+
 import React from 'react';
 import { Bar } from 'react-chartjs-2';
 import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from 'chart.js';
 
-// Register Chart.js components
 ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 
 const RevenueChart = () => {
-
-
-
 
   const revenueData = [500, 700, 800, 600, 750, 900, 650, 870, 960, 1020, 1100, 1150];;
 
@@ -47,7 +43,7 @@ const RevenueChart = () => {
   return (
     <div className="w-full max-w-3xl mx-auto p-4 bg-white shadow-lg rounded-lg">
       <h2 className="text-center text-2xl font-bold text-gray-800 mb-4">Monthly Revenue</h2>
-      <div className='hidden md:block'>
+      <div className='hidden '>
       <Bar data={data} options={options} className='' />
       </div>
     </div>
